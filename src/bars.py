55772f003cc38e4810d31e491cf4b7e082a3cad4
zytdevelop <35646364+zytdevelop@users.noreply.github.com>
2020-03-07 21:09:17 +0800

    Create bars.py

---
+++ src/bars.py
@@ -0,0 +1,33 @@
+"""
+学习python模块知识点的示例
+
+定义个.py 后缀文件,然后用 import 语句导入模块
+import bars
+
+"""
+
+def starbar(num):
+    """ 打印 * 分割线
+    
+    :arg num: 线长
+    """
+    print('*' * num)
+    return None
+
+def hashbar(num):
+    """ 打印 # 分割线
+    
+    :arg num: 线长
+    """
+    print('#' * num)
+    return None
+    
+def simplestar(num):
+    """ 打印 - 分割线
+    
+    : arg num : 线长
+    """
+    print('-' * num)
+    return None
+    
+ 
