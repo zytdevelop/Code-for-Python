f6fd9ee969fb07422359364d833a009bd1d23fd1
zytdevelop <522396766@qq.com>
2021-01-12 14:59:47 +0800

    python 两个排序函数效率比较(2)

--- src/sort_function_test.py
+++ src/sort_function_test.py
@@ -3,19 +3,27 @@ import random as rd
 num_list = [rd.random() for i in range(0, 200000)]
 
 # 测试列表的sort()函数与内置函数sorted()的效率
-for i in range(0, 1000):
+sort_total_time = 0
+sorted_total_time = 0
+test_count = 1000
+for i in range(0, test_count):
     start = time.time()
     num_list.sort()
     end = time.time()
-    print('==============')
-    print('sort() 函数所用时间为 %d', (end-start))
     sort_time = end-start
-    print('==============')
+    sort_total_time += sort_time
 
     start = time.time()
     new_list = sorted(num_list)
     end = time.time()
-    print('==============')
     sorted_time = (end-start)
-    print('sorted() 函数所用时间为 %d', (end-start))
-    print('==============')
+    sorted_total_time += sorted_time
+
+
+print('==============')
+print('sort() 函数所用时间为 %d', (sort_total_time / test_count))
+print('==============')
+
+print('==============')
+print('sorted() 函数所用时间为 %d', (sorted_total_time / test_count))
+print('==============')
