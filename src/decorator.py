5251aabc6cf24b852df4e1daeac6a8fc3f01c6dc
zytdevelop <522396766@qq.com>
2021-02-26 13:10:30 +0800

    装饰器实验

---
+++ src/decorator.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import time, functools
+
+
+def metric(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        print('%s executed in %s ms' % (fn.__name__, 10.24))
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+# 测试
+@metric
+def fast(x, y):
+    time.sleep(0.0012)
+    return x + y
+
+
+@metric
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z
+
+
+f = fast(11, 22)
+s = slow(11, 22, 33)
+if f != 33:
+    print('测试失败!')
+elif s != 7986:
+    print('测试失败!')
